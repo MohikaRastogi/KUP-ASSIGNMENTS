bbec32c49cc7989d230f0155dea25346b9a4e307
M0HIKA <mohika.rastogi@knoldus.com>
2023-03-15 11:43:02 +0530

    adding factorial using tail recursion program

--- FactorialUsingTailRecursion/src/main/scala/FactorialUsingTailRecursion.scala
+++ FactorialUsingTailRecursion/src/main/scala/FactorialUsingTailRecursion.scala
@@ -5,7 +5,9 @@ object FactorialUsingTailRecursion extends App {
   def factorial(number: Int): Long = {
     @tailrec
     def computeFactorial(number: Int, accumulator: Long): Long = {
-      if (number <= 1)
+      if (number<0)
+        throw new IllegalArgumentException("Cannot find factorial of negative number")
+      else if (number == 0)
         accumulator
       else
         computeFactorial(number - 1, number * accumulator)
--- FactorialUsingTailRecursion/src/test/scala/FactorialUsingTailRecursionTest.scala
+++ FactorialUsingTailRecursion/src/test/scala/FactorialUsingTailRecursionTest.scala
@@ -2,17 +2,22 @@ import org.scalatest.funsuite.AnyFunSuite
 
 class FactorialUsingTailRecursionTest extends AnyFunSuite
 {
-  test("FactorialUsingTailRecursion.factorial()") {
+  test("Test Case to check factorial of 3 is 6 or not") {
     assert(FactorialUsingTailRecursion.factorial(3) === 6)
   }
-  test("FactorialUsingTailRecursion.factorial2()") {
+  test("Test case to check if factorial of 10 is 362880 or not") {
     assert(FactorialUsingTailRecursion.factorial(10) === 3628800)
   }
-  test("FactorialUsingTailRecursion.factorial3()"){
+  test("Test Case to check factorial of 1 is 1 or not"){
     assert(FactorialUsingTailRecursion.factorial(1) === 1)
   }
-  test("FactorialUsingTailRecursion.factorial4()") {
+  test("Test Case to check if factorial of 0 is 1 or not") {
     assert(FactorialUsingTailRecursion.factorial(0) === 1)
+  }
+  test("Test Case for Exception Handling"){        //test case for exception handling
+    assertThrows[IllegalArgumentException] {
+      FactorialUsingTailRecursion.factorial(-1)
+    }
   }
 
 }
