1ad87e11b367b20d42d5573bc40b0f444c500347
M0HIKA <mohika.rastogi@knoldus.com>
2023-03-15 10:59:23 +0530

    updating test file

--- ElectricityCharges/src/test/scala/ElectricityChargesTest.scala
+++ ElectricityCharges/src/test/scala/ElectricityChargesTest.scala
@@ -3,27 +3,31 @@ package com.knoldus
 
 import org.scalatest.funsuite.AnyFunSuite
 
-class ElectricityChargesTest extends AnyFunSuite{
-  test("ElectricityCharges.computeFinalBill")
+class ElectricityChargesTest extends AnyFunSuite {
+  test("ElectricityCharges.computeFinalBillTest1")
   {
-    val bill1 = ElectricityCharges(1001, "user1", "Delhi", 5062, 5904)   // test case given in problem definition
+    val bill1 = ElectricityCharges(1001, "user1", "Delhi", 5062, 5904) // test case given in problem definition
     assert(bill1.computeFinalBillWithGST() == 7073.51)
-
-    val bill2 = ElectricityCharges(1002, "user2", "Mumbai", 200, 350)  //general test case
+  }
+  test("ElectricityCharges.computeFinalBillTest2")
+  {
+    val bill2 = ElectricityCharges(1002, "user2", "Mumbai", 200, 350) //general test case
     assert(bill2.computeFinalBillWithGST() == 929.25)
-
-    val bill3 = ElectricityCharges(1003, "user3", "Bangalore", 400, 900)  //general test case
+  }
+  test("ElectricityCharges.computeFinalBillTest3")
+  {
+    val bill3 = ElectricityCharges(1003, "user3", "Bangalore", 400, 900) //general test case
     assert(bill3.computeFinalBillWithGST() == 3643.25)
-
-    val bill4 = ElectricityCharges(1004, "user4", "Chennai", 200, 200)  //test case for units consumed = 0
+  }
+  test("ElectricityCharges.computeFinalBillTest4")
+  {
+    val bill4 = ElectricityCharges(1004, "user4", "Chennai", 200, 200) //test case for units consumed = 0
     assert(bill4.computeFinalBillWithGST() == 0.0)
-
-    val bill5 = ElectricityCharges(1005, "user5", "Delhi", 300, 200)  //test case for exception handling
+  }
+  test("ElectricityCharges.computeFinalBillTest5")
+  {
+    val bill5 = ElectricityCharges(1005, "user5", "Delhi", 300, 200) //test case for exception handling
     assert(bill5.computeFinalBillWithGST() == 0.0)
-
-
-
-
   }
 
 }
