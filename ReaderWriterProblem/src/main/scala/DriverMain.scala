66e3bf51100bb585b701b21700852253e7558215
M0HIKA <mohika.rastogi@knoldus.com>
2023-04-06 18:54:45 +0530

    adding reader writer

--- ReaderWriterProblem/src/main/scala/DriverMain.scala
+++ ReaderWriterProblem/src/main/scala/DriverMain.scala
@@ -1,42 +1,35 @@
 package com.knoldus
 
-import java.util.concurrent.Executors
-
 object DriverMain extends App {
 
-  // Create a thread pool to execute the tasks concurrently
-  private val executor = Executors.newFixedThreadPool(4)
-  private val readWriteOperation = new ReadWriteOperation
+    private val readWriteOperation = new ReadWriteOperation
 
-  // Submit tasks to the thread pool
-  executor.submit(new Runnable() {
-    def run(): Unit = readWriteOperation.readOperation()
-  })
-  executor.submit(new Runnable() {
-    def run(): Unit = readWriteOperation.readOperation()
-  })
-  executor.submit(new Runnable() {
-    def run(): Unit = readWriteOperation.writeOperation()
-  })
-  executor.submit(new Runnable() {
-    def run(): Unit = readWriteOperation.readOperation()
-  })
-  executor.submit(new Runnable() {
-    def run(): Unit = readWriteOperation.readOperation()
-  })
-  executor.submit(new Runnable() {
-    def run(): Unit = readWriteOperation.writeOperation()
-  })
-  executor.submit(new Runnable() {
-    def run(): Unit = readWriteOperation.writeOperation()
-  })
-  executor.submit(new Runnable() {
-    def run(): Unit = readWriteOperation.readOperation()
-  })
+    // Create threads for each read and write operation
+    private val reader1 = new Thread(() => readWriteOperation.readOperation(), "Reader1")
 
-  // Shutdown the thread pool
-  executor.shutdown()
-}
+    private val reader2 = new Thread(() => readWriteOperation.readOperation(), "Reader2")
 
+    private val writer1 = new Thread(() => readWriteOperation.writeOperation(), "Writer1")
+
+    private val reader3 = new Thread(() => readWriteOperation.readOperation(), "Reader3")
+
+    private val reader4 = new Thread(() => readWriteOperation.readOperation(), "Reader4")
+
+  private val writer2 = new Thread(() => readWriteOperation.writeOperation(), "Writer2")
+
+  private val writer3 = new Thread(() => readWriteOperation.writeOperation(), "Writer3")
+
+    private val reader5 = new Thread(() => readWriteOperation.readOperation(), "Reader5")
+
+    // Start the threads
+    reader1.start()
+    reader2.start()
+    writer1.start()
+    reader3.start()
+    reader4.start()
+    writer2.start()
+    writer3.start()
+    reader5.start()
+  }
 
 
